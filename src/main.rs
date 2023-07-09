4fd98a93b5263ea79da01b0c1dddb6042c9321d5
Alexander Wilkens <alexander.wilkens96@gmail.com>
2023-07-09 11:13:45 +0200

    Add progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use image::{ImageBuffer, Rgb, RgbImage};
+use indicatif::{ProgressBar, ProgressStyle};
 
 fn get_pixel_color(x: u32, y: u32, image_width: u32, image_height: u32) -> Rgb<u8> {
-
     let pct_x = x as f32 / image_width as f32;
     let r = (0xFF as f32 * pct_x) as u8;
 
@@ -17,8 +17,11 @@ fn main() {
 
     let mut image: RgbImage = ImageBuffer::new(image_width, image_height);
 
+    let progress = ProgressBar::new((image_width * image_height) as u64);
+
     for (x, y, pixel) in image.enumerate_pixels_mut() {
         *pixel = get_pixel_color(x, y, image_width, image_height);
+        progress.inc(1);
     }
 
     image.save("/tmp/image.png").unwrap();
