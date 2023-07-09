fc344d9cf3833b02e6ddb0e3ad9e859aebd84ee5
Alexander Wilkens <alexander.wilkens96@gmail.com>
2023-07-09 11:01:28 +0200

    Simple output image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use image::{ImageBuffer, Rgb, RgbImage};
+
+fn get_pixel_color(x: u32, y: u32, image_width: u32, image_height: u32) -> Rgb<u8> {
+
+    let pct_x = x as f32 / image_width as f32;
+    let r = (0xFF as f32 * pct_x) as u8;
+
+    let pct_y = y as f32 / image_height as f32;
+    let g = (0xFF as f32 * pct_y) as u8;
+
+    return Rgb([r, g, 0xFF / 4]);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let image_width: u32 = 256;
+    let image_height: u32 = 256;
+
+    let mut image: RgbImage = ImageBuffer::new(image_width, image_height);
+
+    for (x, y, pixel) in image.enumerate_pixels_mut() {
+        *pixel = get_pixel_color(x, y, image_width, image_height);
+    }
+
+    image.save("/tmp/image.png").unwrap();
 }
